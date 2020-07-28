5b89273de4a2118f92bacd62d1435d43401b8744
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-07-28 16:31:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,35 +2,6 @@
 
 def game_hash {
 
-home:{
-  team_name: ,
-  colors: ,
-  players:[{
-    player_name: ,
-    number: ,
-    shoe: ,
-    rebounds: ,
-    assists: ,
-    steals: ,
-    blocks: ,
-    slam_dunks:
-    }]
-},
-away:{
-  team_name: ,
-  colors: ,
-  players:[{
-    player_name: ,
-    number: ,
-    shoe: ,
-    rebounds: ,
-    assists: ,
-    steals: ,
-    blocks: ,
-    slam_dunks:
-    }]
-}
-
 }
 
 end
